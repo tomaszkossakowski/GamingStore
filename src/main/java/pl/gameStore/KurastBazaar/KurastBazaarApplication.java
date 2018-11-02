81f53ba4d06bd79c54321b4a5dd938497b4afa87
kossakow <tomasz.kossakowski@tomtom.com>
2018-11-02 12:52:52 +0100

    first commit

---
+++ src/main/java/pl/gameStore/KurastBazaar/KurastBazaarApplication.java
@@ -0,0 +1,12 @@
+package pl.gameStore.KurastBazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurastBazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurastBazaarApplication.class, args);
+	}
+}

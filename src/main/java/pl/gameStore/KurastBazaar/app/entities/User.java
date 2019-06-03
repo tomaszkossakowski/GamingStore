01458942e89544979b0fb526de3a9f3d2770e50b
BudynZeSmalcem <pawek47@gmail.com>
2019-06-03 15:27:53 +0200

    codeReview

--- src/main/java/pl/gameStore/KurastBazaar/app/entities/User.java
+++ src/main/java/pl/gameStore/KurastBazaar/app/entities/User.java
@@ -1,50 +1,37 @@
 package pl.gameStore.KurastBazaar.app.entities;
 
-import java.time.ZonedDateTime;
+import lombok.Data;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
-
-import lombok.Data;
+import java.time.ZonedDateTime;
 
 
 @Data
 @Entity
 @Table(name = "user")
-public class User
-{
+public class User {
     @Id
-    @PrimaryKeyJoinColumn
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
 
-    @Column(name = "user_name")
     private String userName;
 
-    @Column(name = "user_login")
     private String userLogin;
 
-    @Column (name = "password")
     private String password;
 
-    @Column (name = "user_info")
     private String userInfo;
 
-    @Column (name = "email")
     private String email;
 
-    @Column (name = "creation_date")
     private ZonedDateTime creationDate;
 
-    @Column (name = "deleted")
     private ZonedDateTime deleted;
 
-    @Column (name = "password_changed")
     private Boolean passwordChanged;
 }
